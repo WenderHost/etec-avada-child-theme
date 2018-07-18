ec4eb0954a426407ab77fad1529b0da1996088c6
Michael Wender <michael@michaelwender.com>
2018-07-18 15:30:42 -0400

    Using minified file

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -3,6 +3,6 @@
 namespace AvadaChild\fns\enqueues;
 
 function enqueue_scripts(){
-  wp_enqueue_style( 'avada-child', get_stylesheet_directory_uri() . '/lib/css/main.css', null, filemtime( get_stylesheet_directory() . '/lib/css/main.css' ) );
+  wp_enqueue_style( 'avada-child', get_stylesheet_directory_uri() . '/lib/css/main.min.css', null, filemtime( get_stylesheet_directory() . '/lib/css/main.css' ) );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
