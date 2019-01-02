3485ccadd30a46be482b90eed1bf67c31a2c6c45
Michael Wender <michael@michaelwender.com>
2019-01-02 14:52:56 -0500

    Dequeuing parent dyanmic css; Adjusting font size

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -3,6 +3,7 @@
 namespace AvadaChild\fns\enqueues;
 
 function enqueue_scripts(){
-  wp_enqueue_style( 'avada-child', get_stylesheet_directory_uri() . '/lib/css/main.min.css', null, filemtime( get_stylesheet_directory() . '/lib/css/main.css' ) );
+  wp_dequeue_style( 'avada-dynamic-css' );
+  wp_enqueue_style( 'avada-child', get_stylesheet_directory_uri() . '/lib/css/main.css', null, filemtime( get_stylesheet_directory() . '/lib/css/main.css' ) );
 }
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts', 20 );
